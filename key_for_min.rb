5d5607b8332c95dd7fc80a7ab826e1ba0f70cda0
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-31 02:56:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_name = nil
   
 name_hash.each do |name, number|
-  if lowest_number == 0 or number < lowest_number
+  if lowest_number == 0 || number < lowest_number
     lowest number = number
     lowest_name = name
   end

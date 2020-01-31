7db528dcc3b2434ed453dd289c8c04dab466d1ea
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-31 02:48:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  name_hash = {
+    :blake => 500,
+    :ashley => 2,
+    :adam => 1
+  }
   
+name_hash.each do |name, index|
+  if index <= 1 
+    name
+  end
+end
 
 end

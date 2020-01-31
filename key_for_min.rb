bc1003fae642b2a5634f98459c9c317a18bb25b0
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-31 02:54:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,8 @@ def key_for_min_value(name_hash)
     :ashley => 2,
     :adam => 1
   }
+  lowest_number = 0
+  lowest_name = nil
   
 name_hash.each do |name, number|
   if lowest_number == 0 or number < lowest_number
